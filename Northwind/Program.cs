b6a38dacf0103dc9e093cf5b465e66a18403e618
Jagoda Sulikowska <jsulikowska@FosterandPartners.com>
2025-01-22 15:55:57 +0100

    added cors settings

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -8,5 +8,15 @@ var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
 
 builder.Services.AddScoped<INorthwindDBRepo, NorthwindDBRepo>();
+builder.Services.AddCors(new Action<Microsoft.AspNetCore.Cors.Infrastructure.CorsOptions>(options =>
+{
+    options.AddDefaultPolicy(new Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicy
+    {
+        Headers = { "Content-Type", "x-functions-key" },
+        Methods = { "GET", "POST", "PUT", "DELETE" },
+        Origins = { "*" }
+    });
+}));
 
 builder.Build().Run();
+
